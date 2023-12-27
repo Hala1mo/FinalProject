4bd7142e14011da39262528669fdd85070f2942f
Hala1mo <halamontaser13@gmail.com>
2023-12-27 15:49:41 +0200

    huuhuhuh

--- app/src/main/java/com/example/finalproject/Book.java
+++ app/src/main/java/com/example/finalproject/Book.java
@@ -1,9 +1,12 @@
 package com.example.finalproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,58 +15,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.Map;
 
+public class Book extends AppCompatActivity {
+    private static final String LOG = "FIREBASE";
+    private EditText txtEmail, txtFname, txtLnamee, phonee, add, pass;
+    private DocumentReference myDoc;
+    SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
 
+    private Button submitBtn;
 
-    public class Book extends AppCompatActivity {
-        private static final String EMAIL_KEY = "Email";
-        private static final String PASSWORD_KEY = "Password";
-        private EditText emailEditText;
-        private EditText passwordEditText;
-        private FirebaseFirestore database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.connect);
+        txtEmail = findViewById(R.id.emailEditText);
+        txtFname = findViewById(R.id.firstNameEditText);
+        txtLnamee = findViewById(R.id.lastNameEditText);
+        phonee = findViewById(R.id.phoneEditText);
+        add = findViewById(R.id.addressEditText);
+        pass = findViewById(R.id.passwordEditText);
+        submitBtn = findViewById(R.id.submitButton);
 
-            emailEditText = findViewById(R.id.edtEmail);
-            passwordEditText = findViewById(R.id.edtPass);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("dfasdfasfas","doneeeeee");
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                String name = txtFname.getText().toString();
+                String email = txtEmail.getText().toString();
+                String Lname = txtLnamee.getText().toString();
+                String phone = phonee.getText().toString();
+                String address = add.getText().toString();
+                String password = pass.getText().toString();
 
-            database = FirebaseFirestore.getInstance();
-        }
+                if (name.isEmpty() || email.isEmpty() || Lname.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty()) {
+                    return;
+                }
 
-        public void btnOnclick(View view) {
-            String email = emailEditText.getText().toString();
-            String password = passwordEditText.getText().toString();
+                Map<String,String> dataToSave = new HashMap<>();
+                dataToSave.put("name", name);
+                dataToSave.put("email", email);
+                dataToSave.put("Lname", Lname);
+                dataToSave.put("phone", phone);
+                dataToSave.put("address", address);
+                dataToSave.put("password", password);
 
-            if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show();
-                return;
+                Gson gson = new Gson();
+                Map<String,String>users = new HashMap<>();
+                users.put("email",email);
+                users.put("pass",password);
+                String usersjson = gson.toJson(users);
+                sharedPreferences.edit().putString("user", usersjson).apply();
+
+                db.collection("sampleData").document().set(dataToSave)
+                        .addOnSuccessListener(new OnSuccessListener() {
+                            @Override
+                            public void onSuccess(Object o) {
+                                String message = email;
+                                Intent intent = new Intent(Book.this, ActivRe.class);
+                                intent.putExtra("message", message);
+                                startActivity(intent);
+                                Toast.makeText(Book.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                                Log.d(LOG, "Successfully ");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Book.this, "Error", Toast.LENGTH_SHORT).show();
+                                Log.d(LOG, "Document has not been updated:)");
+                            }
+                        });
             }
+        });
 
-            Map<String, String> userMap = new HashMap<>();
-            userMap.put(EMAIL_KEY, email);
-            userMap.put(PASSWORD_KEY, password);
 
-            database.collection("Register").document("users")
-                    .set(userMap)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Toast.makeText(Book.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(Book.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-                            Log.e("Book", "Error registering user", e);
-                        }
-                    });
-        }
     }
+
+}
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,18 +2,35 @@ package com.example.finalproject;
 
 import static android.system.Os.connect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.appsearch.PutDocumentsRequest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.Manifest;
+
+
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ktx.Firebase;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,25 +42,35 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton Dining;
     private ImageButton room;
     private ImageButton book;
+    private ImageButton map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         callButton = findViewById(R.id.idImgBtn7);
         gallery=findViewById(R.id.idImgBtn3);
         about=findViewById(R.id.idImgBtn);
         Dining=findViewById(R.id.idImgBtn6);
         room=findViewById(R.id.idImgBtn5);
         book=findViewById(R.id.idImgBtn2);
+        map=findViewById(R.id.idImgBtn4);
 
-        callButton.setOnClickListener(new View.OnClickListener() {
+       /* callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 makePhoneCall();
             }
+        });*/
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                map();
+            }
         });
+
+
 
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         Dining.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                makeactivityyy();
+                dining();
             }
         });
 
@@ -71,36 +98,33 @@ public class MainActivity extends AppCompatActivity {
                 makeactivityyy();
             }
         });
-        book.setOnClickListener(new View.OnClickListener() {
+       book.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
               make();
             }
+
+
         });
-    }
-
-
-    private void makePhoneCall() {
-        String phoneNumber = "123456789"; // Replace with your desired phone number
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
-        } else {
-            Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse("tel:" + phoneNumber));
-            startActivity(intent);
-        }
     }
     public void makeactivity(){
         Intent intent = new Intent (this,Activity.class);
         startActivity(intent);
     }
+    public void map(){
+        Intent intent = new Intent (this,map.class);
+        startActivity(intent);
+    }
+    public void dining(){
+        Intent intent = new Intent (this,Dining.class);
+        startActivity(intent);
+    }
     public void make(){
-        Intent intent = new Intent (this,Book.class);
+        Intent intent = new Intent (this,Loginn.class);
         startActivity(intent);
     }
     public void makeactivityyy(){
-        Intent intent = new Intent (this,Dining.class);
+        Intent intent = new Intent (this,room.class);
         startActivity(intent);
     }
     public void makeactivityy(){
@@ -108,7 +132,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
+
+   /* @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CALL_PHONE) {
@@ -116,9 +141,7 @@ public class MainActivity extends AppCompatActivity {
                 makePhoneCall();
             }
         }
-    }
-
-
+    }*/
 
 
 }
